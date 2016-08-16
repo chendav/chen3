c2630ff3e666466b3e3880cae7d38930564eb074
chendav <chen00300@outlook.com>
2016-08-16 15:50:40 +0800

    many2

--- chen3/spiders/chen3_spider.py
+++ chen3/spiders/chen3_spider.py
@@ -3,16 +3,28 @@
 
 from scrapy.spider import Spider
 from scrapy.selector import Selector
+from scrapy.http import Request
+from scrapy.spider import Rule,CrawlSpider
+from scrapy.linkextractors import LinkExtractor
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 
 from chen3.items import Chen3Item
 
-class chen3Spider(Spider):
+class chen3Spider(CrawlSpider):
 	name = "chen"
+	download_delay=1
 	allowed_domains=["caigou.my.gov.cn"]
 	start_urls=[
 		"http://caigou.my.gov.cn/ceinwz/WebInfo_List.aspx?newsid=601&jsgc=&zfcg=0100000&tdjy=&cqjy=&PubDateSort=\
 		0&ShowPre=1&CbsZgys=&zbfs=&qxxx=&showqxname=0&NewsShowPre=1&wsjj=&showCgr=0&ShowOverDate=&FromUrl=sjjyxx"
 		]
+	rules=[
+		Rule(SgmlLinkExtractor(allow=('http://caigou.my.gov.cn/ceinwz/WebInfo_List.aspx?newsid=601&jsgc=&zfcg=0100000&tdjy\
+		=&cqjy=&PubDateSort=0&ShowPre=1&CbsZgys=&zbfs=&qxxx=&showqxname=0&NewsShowPre=1&wsjj=&showCgr=0&ShowOverDate\
+		=&FromUrl=sjjyxx')),
+		     callback='parse',
+		     follow=True)
+	]
 	def parse(self,response):
 		sel=Selector(response)
 		sites=sel.xpath('//a[@id]/text()')
